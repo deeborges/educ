347ee2655d21fc38ad03184c3d8270bffe9f8db2
deeborges <deyvisonsilvaborges@gmail.com>
2020-04-24 20:48:06 -0300

    various layouts for application

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="gce__educacional">
+    <nuxt/>
+  </main>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.gce__educacional {
+  position: relative;
+}
+</style>
