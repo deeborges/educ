212448f85e1a97041ebe40dbf7b71408d4d8fd77
Deyvison Borges <web.dborges@gmail.com>
2020-04-25 20:57:09 -0300

    fix.: component dinamico -> button

---
+++ components/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :type=type>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      String: true,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style>
+button {
+  background-color: #9d9d9d;
+  border: none;
+  border-radius: 10px;
+  padding: 10px 20px;
+  position: absolute;
+}
+</style>
