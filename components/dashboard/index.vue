53230e10c29ae4421bc31dcef9f2f3e34a7dc083
Deyvison Borges <web.dborges@gmail.com>
2020-04-28 13:08:15 -0300

    fix > organizando layout do dashboard

---
+++ components/dashboard/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <DashboardContainer>
+    <Sidebar>
+      <SidebarOptions />
+    </Sidebar>
+    <DashboardContent>
+      <Navbar>
+        <NavbarProfile />
+      </Navbar>
+      <nuxt-child></nuxt-child>
+    </DashboardContent>
+  </DashboardContainer>
+</template>
+
+<script>
+import DashboardContainer from "../dashboard/dashboard-container";
+import DashboardContent from "../dashboard/dashboard-content";
+import Sidebar from "../../components/sidebar/sidebar";
+import SidebarOptions from "../../components/sidebar/options";
+import Navbar from "../../components/navbar/navbar";
+import NavbarProfile from "../../components/navbar/navProfile";
+
+export default {
+  name: "PageDashboard",
+  layout: "default",
+  components: {
+    DashboardContainer,
+    DashboardContent,
+    Sidebar,
+    SidebarOptions,
+    Navbar,
+    NavbarProfile,
+  }
+};
+</script>
