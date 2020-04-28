615f9d191689c182e289d2e1fd911112f072616f
Deyvison Borges <web.dborges@gmail.com>
2020-04-28 20:20:12 -0300

    not monitored

---
+++ components/navbar/navbar-container.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="gce__navbar">
+    <slot></slot>
+  </nav>
+</template>
+
+<style>
+@import url('~/assets/navbar/navbar-container.css');
+</style>
---
+++ components/navbar/navbar-profile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="gce__navbar--profile">
+    <div>imagem</div>
+    <p>Deyvison</p>
+    <i>></i>
+  </div>
+</template>
+
+<style>
+@import url('~/assets/navbar/navbar-profile.css');
+</style>
