1aea36de7e60e10c638a9cfdfeca9790006f771c
Deyvison Borges <web.dborges@gmail.com>
2020-04-28 13:09:37 -0300

    dev > projetando botão de submit

---
+++ components/button-submit.vue
@@ -0,0 +1,31 @@
+<template>
+  <button 
+  :type=type
+    class="button__submit"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style>
+.button__submit {
+  background-color: #6f6f6f;
+  border: none;
+  border-radius: 10px;
+  color: #ffffff;
+  height: fit-content;
+  padding: 10px 20px;
+  width: auto;
+}
+</style>
