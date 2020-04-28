685e4440d19d4373611b7022d6cd3589ea6503cf
Deyvison Borges <web.dborges@gmail.com>
2020-04-28 13:07:42 -0300

    fix > organização do layout do profile

---
+++ components/dashboard/profile/profile-form-address.vue
@@ -0,0 +1,116 @@
+<template>
+  <form class="form__profile--address">
+    <p class="desc__params">Endereço: </p>
+    <div class="grid__06">
+      <label for="logradouro">Logradouro: </label>
+      <input type="text" name="logradouro" />
+      <label for="complemento">Complemento: </label>
+      <input type="text" name="complemento" />
+      <label for="numero_casa">nº: </label>
+      <input type="text" name="numero_casa" />
+    </div>
+
+    <div class="grid__06">
+      <label for="bairro">Bairro: </label>
+      <input type="text" name="bairro" />    
+      <label for="cidade">Cidade: </label>
+      <input type="text" name="cidade" />
+      <label for="estados_endereco">Estado: </label>
+      <select name="estados_endereco">
+        <option value="AC">Acre</option>
+        <option value="AL">Alagoas</option>
+        <option value="AP">Amapá</option>
+        <option value="AM">Amazonas</option>
+        <option value="BA">Bahia</option>
+        <option value="CE">Ceará</option>
+        <option value="DF">Distrito Federal</option>
+        <option value="ES">Espírito Santo</option>
+        <option value="GO">Goiás</option>
+        <option value="MA">Maranhão</option>
+        <option value="MT">Mato Grosso</option>
+        <option value="MS">Mato Grosso do Sul</option>
+        <option value="MG">Minas Gerais</option>
+        <option value="PA">Pará</option>
+        <option value="PB">Paraíba</option>
+        <option value="PR">Paraná</option>
+        <option value="PE">Pernambuco</option>
+        <option value="PI">Piauí</option>
+        <option value="RJ">Rio de Janeiro</option>
+        <option value="RN">Rio Grande do Norte</option>
+        <option value="RS">Rio Grande do Sul</option>
+        <option value="RO">Rondônia</option>
+        <option value="RR">Roraima</option>
+        <option value="SC">Santa Catarina</option>
+        <option value="SP">São Paulo</option>
+        <option value="SE">Sergipe</option>
+        <option value="TO">Tocantins</option>
+      </select>
+      </div>
+
+  </form>
+</template>
+
+<style scoped>
+.form__profile--address {
+  background-color: #fdfdfd;
+  border-radius: 5px;
+  box-shadow: 0 0 5px 1px #d9d9d9;
+  display: grid;
+  gap: 10px;
+  height: fit-content;
+  margin: 15px 0;
+  padding: 20px;
+  position: relative;
+  width: auto;
+}
+
+.form__profile--address label {
+  align-content: center;
+  display: grid;
+  font-size: 1rem;
+  position: relative;
+}
+
+.form__profile--address input {
+  font-size: 1rem;
+  height: 42px;
+  min-width: 20px;
+  position: relative;
+  width: auto;
+}
+
+.form__profile--address input[name='numero_casa'] {
+  width: 50px;
+}
+
+.form__profile--address select {
+  font-size: 1rem;
+  height: auto;
+  position: relative;
+}
+
+.form__profile--address select option {
+  font-size: 1rem;
+  position: relative;
+}
+
+.grid__06 {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-template-columns: repeat(3, auto  auto);
+  gap: 10px;
+  position: relative;
+  width: auto;
+}
+
+.desc__params {
+  color: #797979;
+  font-size: 1.2rem;
+  position: relative;
+}
+
+.button {
+  display: grid;
+  position: relative;
+}
+</style>
---
+++ components/dashboard/profile/profile-form-personal-data.vue
@@ -0,0 +1,143 @@
+<template>
+  <form class="form__profile--personal__data">
+    <p class="desc__params">Dados pessoais</p>
+    <div class="grid__02">
+      <label for="nome_completo">Nome completo:</label>
+      <input type="text" name="nome_completo" />
+    </div>
+    <div class="grid__04">
+      <label for="cpf">CPF:</label>
+      <TheMask :mask="'###.###.###-##'" name="cpf" />
+      <label for="rg">RG:</label>
+      <TheMask :mask="'############-#'" name="rg" />
+    </div>
+    <div class="grid__06">
+      <label for="data_nascimento">Data de nascimento:</label>
+      <TheMask type="tel" :mask="'##/##/####'" name="data_nascimento" />
+      <label for="naturalidade">Naturalidade:</label>
+      <input type="text" name="naturalidade" />
+      <label for="estados_dados_pessoais">Estado:</label>
+      <select name="estados_dados_pessoais">
+        <option value="AC">Acre</option>
+        <option value="AL">Alagoas</option>
+        <option value="AP">Amapá</option>
+        <option value="AM">Amazonas</option>
+        <option value="BA">Bahia</option>
+        <option value="CE">Ceará</option>
+        <option value="DF">Distrito Federal</option>
+        <option value="ES">Espírito Santo</option>
+        <option value="GO">Goiás</option>
+        <option value="MA">Maranhão</option>
+        <option value="MT">Mato Grosso</option>
+        <option value="MS">Mato Grosso do Sul</option>
+        <option value="MG">Minas Gerais</option>
+        <option value="PA">Pará</option>
+        <option value="PB">Paraíba</option>
+        <option value="PR">Paraná</option>
+        <option value="PE">Pernambuco</option>
+        <option value="PI">Piauí</option>
+        <option value="RJ">Rio de Janeiro</option>
+        <option value="RN">Rio Grande do Norte</option>
+        <option value="RS">Rio Grande do Sul</option>
+        <option value="RO">Rondônia</option>
+        <option value="RR">Roraima</option>
+        <option value="SC">Santa Catarina</option>
+        <option value="SP">São Paulo</option>
+        <option value="SE">Sergipe</option>
+        <option value="TO">Tocantins</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+<script>
+import { TheMask } from "vue-the-mask";
+
+export default {
+  components: {
+    TheMask
+  }
+};
+</script>
+
+<style scoped>
+.form__profile--personal__data {
+  background-color: #fdfdfd;
+  border-radius: 5px;
+  box-shadow: 0 0 5px 1px #d9d9d9;
+  display: grid;
+  height: fit-content;
+  gap: 10px;
+  margin: 15px 0;
+  padding: 20px;
+  position: relative;
+  width: auto;
+}
+
+.form__profile--personal__data label {
+  align-content: center;
+  display: grid;
+  font-size: 1rem;
+  position: relative;
+}
+
+.form__profile--personal__data input {
+  font-size: 1rem;
+  height: 42px;
+  min-width: 20px;
+  position: relative;
+  width: auto;
+}
+
+.form__profile--personal__data input[name="nome_completo"] {
+  font-size: 1rem;
+  position: relative;
+  width: auto;
+  min-width: 200px;
+}
+
+.form__profile--personal__data select {
+  font-size: 1rem;
+  height: auto;
+  position: relative;
+}
+
+.form__personal__data select option {
+  font-size: 1rem;
+  position: relative;
+}
+
+.grid__02 {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-template-columns: auto 1fr;
+  gap: 10px;
+  position: relative;
+  width: auto;
+}
+
+.grid__04 {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-template-columns: repeat(2, auto 1fr);
+  gap: 10px;
+  position: relative;
+  width: auto;
+}
+
+.grid__06 {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-template-columns: repeat(3, auto auto);
+  gap: 10px;
+  position: relative;
+  width: auto;
+}
+
+.desc__params {
+  color: #797979;
+  display: grid;
+  font-size: 1.2rem;
+  position: relative;
+}
+</style>
